13cf92ec6dd165fd326a682efc59e6bf78249a30
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-12-01 14:14:17 +0000

    Create package util and class to format DateTime

--- src/main/java/pt/academy/controller/AnimeController.java
+++ src/main/java/pt/academy/controller/AnimeController.java
@@ -1,19 +1,30 @@
 package pt.academy.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.academy.domain.Anime;
+import pt.academy.util.DateUtil;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
+
+    private final DateUtil dateUtil;
 
     @GetMapping(path = "list")
     public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
         return List.of(new Anime("One Push Man"), new Anime("Buko No Hero"));
     }
 
